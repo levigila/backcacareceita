24ee34af6d846524f2aa29541c330de74505788f
Isabela Silva <isabelasilvap778@gmail.com>
2024-04-20 03:18:10 -0300

    Correção urls

--- cacareceita/usuarios/urls.py
+++ cacareceita/usuarios/urls.py
@@ -18,7 +18,6 @@ urlpatterns = [
     path('loginerror/', views.login_error, name='loginerror'),
     path('buscaReceitaAprendiz/', views.buscaReceitaAprendiz, name='buscaReceitaAprendiz'),
     path('loginerror/', views.login_error, name='loginerror')  ,  
-    path('receita/', views.receita, name='receita'),
     path('loginerror/', views.login_error, name='loginerror'),
     path('buscaReceitaAprendiz/', views.buscaReceitaAprendiz, name='buscaReceitaAprendiz'),
     path('buscaReceitaMiniChef/', views.buscaReceitaMiniChef, name='buscaReceitaMiniChef'),
