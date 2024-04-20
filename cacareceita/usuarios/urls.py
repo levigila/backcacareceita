b2daef03bb1ae8ce18d7686e31875fc74a0d8647
Isabela Silva <isabelasilvap778@gmail.com>
2024-04-19 21:28:27 -0300

    Correção urls.py

--- cacareceita/usuarios/urls.py
+++ cacareceita/usuarios/urls.py
@@ -6,11 +6,8 @@ urlpatterns = [
     path ('cadastro/', views.cadastro, name='cadastro'),
     path ('login/', views.login, name='login'),
     path ('plataforma/', views.plataforma, name='PlataForma'),
-<<<<<<< HEAD
     path('loginerror/', views.login_error, name='loginerror')  ,  
-    path('receita/', views.receita, name='receita')    
-=======
+    path('receita/', views.receita, name='receita'),
     path('loginerror/', views.login_error, name='loginerror'),
     path('buscaReceitaAprendiz/', views.buscaReceitaAprendiz, name='buscaReceitaAprendiz')
->>>>>>> ab907189a13bceb5afd81d50966ca12ac66f1d4d
 ]
